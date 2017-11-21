b2bb3a3fd2fb847c7b1168437ee18cfaaeaf5311
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-21 02:27:08 +0000

    When the user selects a Radio it should display the price and number of available rooms for that object A checkmark should be included to have the user agree to the terms and conditions.
    The page should validate that a room was indeed selected and the conditions aggreed. Then it should reserve the room with a placeholder name.

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,14 @@ var hotel = {
             name: "Luxery Suite",
             price: "$250.00",
             available: 5
+        },
+        {
+            name: "Penthouse w/ pool",
+            price: "$600.00",
+            available: 2
         }
     ],
-    name: "CareerDevs Innt"
+    name: "CareerDevs Innt",
 }
 
 // console.log(hotel.rooms[1].price);
@@ -24,14 +29,47 @@ var hotel = {
 // console.log(document.getElementById('radio').nodeName);
 // console.log(document.getElementById('label').nodeName);
 
+function displayDetails(room) {
+    console.log(room);
+    document.getElementById('price').innerHTML = hotel.rooms[room].price;
+    document.getElementById('available').innerHTML = hotel.rooms[room].available;
+}
+
 for (var i = 0; i < hotel.rooms.length; i++) {
+    console.log('start');
    var radioBtn = document.createElement("INPUT");
    radioBtn.setAttribute('type', 'radio');
    radioBtn.setAttribute('name', 'roomType');
    radioBtn.setAttribute('value', i);
    radioBtn.setAttribute('id', 'room' + i);
+//   radioBtn.onclick = displayDetails(i);
+    radioBtn.setAttribute('onclick', "displayDetails(" + i + ")");
    var radioLbl = document.createElement('LABEL');
    radioLbl.innerHTML = hotel.rooms[i].name;
    document.getElementById('radialSection').appendChild(radioBtn);
    document.getElementById('radialSection').appendChild(radioLbl);
+   console.log('stop');
+}
+
+document.getElementById('reservationForm').onsubmit = function (event) {
+    event.preventDefault();
+    if (!document.getElementById('confirmation').checked) {
+        alert('Please agree to terms');
+        return;
+}
+    var radios = document.getElementsByName('roomType');
+    var roomSelected = "";
+    for (i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            roomSelected = radios[i].value;
+            break;
+        }
+    }
+    
+    if(roomSelected == "") {
+        alert('Pick a room please');
+        return;
+    }
+    
+    alert ('You have reserved room ' + hotel.rooms[parseInt(roomSelected)].name + ".");
 }
